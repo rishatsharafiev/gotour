36ab50cb125fa8db4da3e9ee237a5cde8a68624d
Rishat Sharafiev <rishatsharafiev@ya.ru>
2018-06-29 15:52:40 +0300

    Update sum billion with parallel execution test, refactor previous code

--- sum_billion.go
+++ sum_billion.go
@@ -3,15 +3,63 @@ package main
 import (
 	"fmt"
 	"time"
+	"math"
 )
 
-func main() {
+func testSimultaneous(k int) (int, time.Duration) {
+	n := float64(k)
+	startTime := time.Now()
 	sum := 0
-	for i := 0; i < 1000000000; i++ {
+	for i := 0; i < int(n); i++ {
 		sum += i
 	}
-	fmt.Println("Hello, 世界")
-	duration := time.Since(time.Now())
-	fmt.Println("Sum: ", sum)
-	fmt.Println("Delta: ", duration)
+
+	return sum, time.Since(startTime)
+}
+
+func summaryRoutine(from int, to int, c chan int) {
+	sum := 0
+	for i := from; i < to; i++ {
+		sum += i
+	}
+	c <- sum
+}
+
+func testParallel(k int) (int, time.Duration)  {
+	n := float64(k)
+	startTime := time.Now()
+	sum := 0
+
+	for j := 0; j < 1; j++ {
+		step := 25.0
+		size := int(math.Ceil(n / step))
+		c := make(chan int)
+		var from, to int
+
+		for i := 0; i < int(step); i++ {
+			from = i * size
+			to = (i + 1) * size
+			if i == int(step)-1 {
+				to = int(n)
+			}
+			go summaryRoutine(from, to, c)
+		}
+
+		for i := 0; i < int(step); i++ {
+			sum += <-c
+		}
+	}
+
+	return sum, time.Since(startTime)
+}
+
+func main() {
+	k := 15000000000
+
+	s1, d1 := testSimultaneous(k)
+	fmt.Println("testSimultaneous(sum, duration): ", s1, d1)
+
+	s2, d2 := testParallel(k)
+	fmt.Println("testConcurency(sum, duration): ", s2, d2)
+
 }
