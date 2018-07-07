00fc9a6add25d5c0996cdb15c43f693e0abbf3e0
Rishat Sharafiev <rishatsharafiev@ya.ru>
2018-07-07 20:12:33 +0300

    Add Vertex struct (1/26)

---
+++ methods.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+// (1/26) методы
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs () float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	// (1/26) методы
+	v := Vertex{X: 3, Y: 4}
+	fmt.Println(v.Abs())
+}
