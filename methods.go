69160b455b8dcc45ab494afa518aa85e1056282e
Rishat Sharafiev <rishatsharafiev@ya.ru>
2018-07-07 20:16:29 +0300

    Add explicit call of function as struct method

--- methods.go
+++ methods.go
@@ -14,8 +14,13 @@ func (v Vertex) Abs () float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func AbsSecond (v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func main() {
 	// (1/26) методы
 	v := Vertex{X: 3, Y: 4}
 	fmt.Println(v.Abs())
+	fmt.Println(AbsSecond(v))
 }
